c9d56dc25126e3c06fdf37a953365c9166c539e6
amna <antoniomiguel2001@hotmail.es>
2022-12-22 12:58:47 +0100

    .

--- src/main/java/model/Main.java
+++ src/main/java/model/Main.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.io.IOException;
+import java.util.Objects;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -13,7 +14,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         try {
             Parent root;
-            root = FXMLLoader.load(getClass().getResource("view/interfaz.fxml"));
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("view/interfaz.fxml")));
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
             primaryStage.show();
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,5 @@ module TestScreenBuilderFX {
     requires javafx.controls;
     requires javafx.fxml;
     opens model to javafx.graphics, javafx.fxml;
-    opens view to javafx.graphics, javafx.fxml;
     opens controller to javafx.graphics, javafx.fxml;
 }
